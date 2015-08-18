ecc69b9b47609cc51cd197a1ec96f2ac06014740
Andrei Miulescu <andrei@housemeerkat.com>
2015-08-18 17:26:17 +1000

    Component for css flag

--- modules/Flag.js
+++ modules/Flag.js
@@ -1,9 +1,47 @@
 import { Component } from 'react';
+import Radium from 'radium';
 
+@Radium
 class Flag extends Component {
+
+  static propTypes = {
+    formFactor: PropTypes.oneOf(['square', 'normal']).isRequired,
+    countryCode: PropTypes.string.isRequired,
+  }
 
   constructor(props) {
     super(props);
+    this.state = this.props;
+  }
+
+  render() {
+    const styles = {
+      base:{
+        backgroundSize: 'contain',
+        backgroundPosition: '50%',
+        backgroundRepeast: 'no-repeat',
+        position: 'relative',
+        display: 'inline-block',
+        lineHeight: '1em',
+
+        ':before': {
+          content: '\00a0',
+        }
+      },
+      square: {
+        width: '1em',
+      },
+      normal: {
+        width: '1.333333em',
+      },
+      currentFlag: {
+        backgroundImage: url(`../flags/${this.state.formFactor}/${this.state.countryCode}.svg`)
+      }
+    }
+
+    return (
+      <span style={[styles.base, styles[this.state.formFactor], styles.currentFlag]} />
+    );
   }
 
 }
